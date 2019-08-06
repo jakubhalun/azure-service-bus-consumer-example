81aabebd506a3fde184d9450012bdf2ed9c0b8dd
Jakub Hałun <jakubhal@gmail.com>
2019-08-06 18:55:44 +0200

    Initial commit

---
+++ src/main/java/pl/halun/examples/servicebusconsumer/ServiceBusConsumerApplication.java
@@ -0,0 +1,13 @@
+package pl.halun.examples.servicebusconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceBusConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceBusConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/halun/examples/servicebusconsumer/ServiceBusConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.halun.examples.servicebusconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceBusConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
